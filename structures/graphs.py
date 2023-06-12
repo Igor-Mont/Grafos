a53df719b9b482b52919a03ed66a17af9617578b
Uneto47 <netovasconcelos02@gmail.com>
2023-06-12 00:04:45 -0300

    Feat: change to graph

--- structures/graphs.py
+++ structures/graphs.py
@@ -1,51 +1,49 @@
 from linked_list import LinkedList
 
 class Vertex:
-    def __init__(self, index, value):
-        self.index = index
-        self.value = value
-        self.adjacent = LinkedList()
+	def __init__(self, index, value):
+		self.index = index
+		self.value = value
+		self.adjacent = LinkedList()
 
-    def add_adjacent(self, vertex):
-        self.adjacent.append(vertex)
-    
-    def remove_adjacent(self, vertex):
-        self.adjacent.remove(vertex)
-        
-
-    def print_adjacent(self):
-        print(f"Vertex {self.index} - Value: {self.value}")
-        print("adjacent:")
-        for vertex in self.adjacent:
-            print("-> {}".format(vertex.index), end=" ")
-        print("\n")
-
+	def add_adjacent(self, vertex):
+		self.adjacent.append(vertex)
+	
+	def remove_adjacent(self, vertex):
+		self.adjacent.remove(vertex)
+			
+	def print_adjacent(self):
+		print(f"Vertex {self.index} - Value: {self.value}")
+		print("adjacent:")
+		for vertex in self.adjacent:
+				print("-> {}".format(vertex.index), end=" ")
+		print("\n")
 
 class Graphs:
-    def __init__(self):
-        self.vertices = {}
+	def __init__(self):
+		self.vertices = {}
 
-    def add_vertex(self, index, value):
-        vertex = Vertex(index, value)
-        self.vertices[index] = vertex
+	def add_vertex(self, index, value):
+		vertex = Vertex(index, value)
+		self.vertices[index] = vertex
 
-    def add_edge(self, origin, destiny):
-        if origin in self.vertices and destiny in self.vertices:
-            vertex_origin = self.vertices[origin]
-            vertex_destiny = self.vertices[destiny]
-            vertex_origin.add_adjacent(vertex_destiny)
-            vertex_destiny.add_adjacent(vertex_origin)
-            
-    def remove_edge(self, origin, removed):
-        if origin in self.vertices and removed in self.vertices:
-            vertex_origin = self.vertices[origin]
-            vertex_removed = self.vertices[removed]
-            vertex_origin.remove_adjacent(vertex_removed)
-            vertex_removed.remove_adjacent(vertex_origin)
+	def add_edge(self, origin, destiny):
+		if origin in self.vertices and destiny in self.vertices:
+				vertex_origin = self.vertices[origin]
+				vertex_destiny = self.vertices[destiny]
+				vertex_origin.add_adjacent(vertex_destiny)
+				vertex_destiny.add_adjacent(vertex_origin)
+					
+	def remove_edge(self, origin, removed):
+		if origin in self.vertices and removed in self.vertices:
+				vertex_origin = self.vertices[origin]
+				vertex_removed = self.vertices[removed]
+				vertex_origin.remove_adjacent(vertex_removed)
+				vertex_removed.remove_adjacent(vertex_origin)
 
-    def imprimir_graphs(self):
-        for vertex in self.vertices.values():
-            vertex.print_adjacent()
+	def imprimir_graphs(self):
+		for vertex in self.vertices.values():
+				vertex.print_adjacent()
 
 # Exemplo de uso
 graphs = Graphs()
