dfd9c831087d70f270f2c742d152d4ee49d0829e
Igor Nathan Monteiro Santos <202100011495@office.ufs.br>
2023-08-07 16:38:33 -0300

    wip lab 05

---
+++ implementations/lab05/graph.py
@@ -0,0 +1,369 @@
+from linked_list import LinkedList
+
+class Vertex:
+  def __init__(self, data, index):
+    self.data = data
+    self.index = index
+    self.degree = 0
+
+class Graph:
+  def __init__(self, representation_type):
+    self.representation_type = representation_type
+    self._edge_count = 0
+    self.is_representation_list = representation_type == "list"
+    if not self.is_representation_list:
+      self.matrix = list()
+      self.edges_matrix = list()
+      self.vertices_matrix = list()
+      return
+    elif self.is_representation_list:
+      self.adjacency_list = dict()
+      self.edges_list = list()
+    else:
+      raise ValueError("Invalid representation type. Only 'matrix' or 'list' representation is supported.")
+
+  def add_vertex(self, vertex):
+    if self.is_representation_list:
+      self._add_vertex_list(vertex)
+    else:
+      self._add_vertex_matrix(vertex)
+    
+  def _add_vertex_list(self, vertex):
+    if vertex not in self.adjacency_list:
+      self.adjacency_list[vertex] = LinkedList()
+      
+  def _add_vertex_matrix(self, vertex):
+    if vertex not in self.vertices_matrix:
+      self.vertices_matrix.append(vertex)
+      self.matrix.append([0] * len(self.vertices_matrix))
+      for row in self.matrix[:-1]:
+        row.append(0)
+
+  def get_edge_count(self):
+    return self._edge_count
+
+  def add_edge(self, vertex1, vertex2):
+    if self.is_representation_list:
+      self._add_edge_list(vertex1, vertex2)
+    else:
+      self._add_edge_matrix(vertex1, vertex2)
+
+  def _add_edge_list(self, vertex1, vertex2):
+    if vertex1 in self.adjacency_list and vertex2 in self.adjacency_list:
+      self._update_vertex_degrees(vertex1, vertex2)
+      self.adjacency_list[vertex1].append(vertex2)
+      self.adjacency_list[vertex2].append(vertex1)
+      edge = (vertex1.data, vertex2.data)
+      self.edges_list.append(edge)
+      self._edge_count += 1
+    else:
+      raise ValueError("One or both vertexes do not exist in the graph.")
+    
+  def _add_edge_matrix(self, vertex1, vertex2):
+    if vertex1 in self.vertices_matrix and vertex2 in self.vertices_matrix:
+      index1, index2 = self._get_vertices_matrix_index(vertex1, vertex2)
+      self._update_vertex_degrees(vertex1, vertex2)
+      self._update_matrix_entries(index1, index2)
+      edge = (vertex1.data, vertex2.data)
+      self.edges_matrix.append(edge)
+      self._edge_count += 1
+    else:
+      raise ValueError("One or both vertexes do not exist in the graph.")
+  
+  def _update_vertex_degrees(self, vertex1, vertex2, increment=True):
+    if increment:
+      vertex1.degree += 1
+      vertex2.degree += 1
+    else:
+      vertex1.degree -= 1
+      vertex2.degree -= 1
+
+  def _update_matrix_entries(self, index1, index2, increment=True):
+    if increment:
+      self.matrix[index1][index2] += 1
+      self.matrix[index2][index1] += 1
+    else:
+      self.matrix[index1][index2] -= 1
+      self.matrix[index2][index1] -= 1
+      
+  def _get_vertices_matrix_index(self, vertex1, vertex2):
+    index1 = self.vertices_matrix.index(vertex1)
+    index2 = self.vertices_matrix.index(vertex2)
+    return index1, index2
+
+  def _get_vertices_matrix_index(self, vertex1, vertex2):
+    index1 = self.vertices_matrix.index(vertex1)
+    index2 = self.vertices_matrix.index(vertex2)
+    return index1, index2
+
+  def get_vertices(self, only_data=True):
+    if self.is_representation_list:
+      return self._get_vertices_list(only_data)
+    else:
+      return self._get_vertices_matrix(only_data)
+  
+  def _get_vertices_list(self, only_data):
+    if only_data:
+      return [vertex.data for vertex in self.adjacency_list.keys()]
+    return [vertex for vertex in self.adjacency_list.keys()]
+
+  def _get_vertices_matrix(self, only_data):
+    if only_data:
+      return [vertex.data for vertex in self.vertices_matrix]
+    return [vertex for vertex in self.vertices_matrix]
+
+  def get_edges(self, filtered=False):
+    if self.is_representation_list:
+      return self._get_edges_list(filtered)
+    else:
+      return self._get_edges_matrix(filtered)
+    
+  def _get_edges_list(self,  filtered=False):
+    filtered_edges = []
+    if filtered:
+      for edge in self.edges_list:
+        sorted_edge = tuple(sorted(edge))
+        if sorted_edge not in filtered_edges:
+          filtered_edges.append(edge)
+      return filtered_edges
+    return self.edges_list
+  
+  def _get_edges_matrix(self, filtered=False):
+    filtered_edges = []
+    if filtered:
+      for edge in self.edges_matrix:
+        sorted_edge = tuple(sorted(edge))
+        if sorted_edge not in filtered_edges:
+          filtered_edges.append(edge)
+      return filtered_edges
+    return self.edges_matrix
+
+  def has_edge(self, vertex1, vertex2):
+    if self.is_representation_list:
+      return self._has_edge_list(vertex1, vertex2)
+    else:
+      return self._has_edge_matrix(vertex1, vertex2)
+
+  def _has_edge_list(self, vertex1, vertex2):
+    if vertex1 in self.adjacency_list and vertex2 in self.adjacency_list:
+      return vertex2 in self.adjacency_list[vertex1] and vertex1 in self.adjacency_list[vertex2]
+    return False
+  
+  def _has_edge_matrix(self, vertex1, vertex2):
+    if(self.vertices_matrix[0].index == 0):
+      return self.matrix[vertex1.index][vertex2.index] >= 1
+      
+    return self.matrix[vertex1.index-1][vertex2.index-1] >= 1
+
+  def neighboring_vertices(self, index_v1, index_v2):
+    if self.is_representation_list:
+      return self._neighboring_vertices_list(index_v1, index_v2)
+    else:
+      return self._neighboring_vertices_matrix(index_v1, index_v2)
+  
+  def _neighboring_vertices_list(self, index_v1, index_v2):
+    vertex1 = None
+    vertex2 = None
+    for vertex in self.adjacency_list:
+      if vertex.index == index_v1:
+        vertex1 = vertex
+      if vertex.index == index_v2:
+        vertex2 = vertex
+
+    if vertex1 in self.adjacency_list and vertex2 in self.adjacency_list:
+      return vertex2 in self.adjacency_list[vertex1] and vertex1 in self.adjacency_list[vertex2]
+    return False
+
+  def _neighboring_vertices_matrix(self, index_v1, index_v2):
+    if(self.vertices_matrix[0].index == 0):
+      return self.matrix[index_v1][index_v2] >= 1
+      
+    return self.matrix[index_v1-1][index_v2-1] >= 1
+  
+  def vertex_degree(self, index):
+    if self.is_representation_list:
+      return self._vertex_degree_list(index)
+    else:
+      return self._vertex_degree_matrix(index)
+    
+  def _vertex_degree_list(self, index):
+    for vertex in self.adjacency_list:
+      if vertex.index == index:
+        return vertex.degree
+    raise IndexError("list index out of range")
+  
+  def get_vertex_by_data(self, data):
+    if self.is_representation_list:
+      return self._get_vertex_by_data_list(data)
+    else:
+      return self._get_vertex_by_data_matrix(data)
+    
+  def _get_vertex_by_data_list(self, data):
+    for vertex in self.adjacency_list:
+      if vertex.data == data:
+        return vertex
+    return None
+  
+  def _get_vertex_by_data_matrix(self, data):
+    for vertex in self.vertices_matrix:
+      if vertex.data == data:
+        return vertex
+    return None
+
+  def get_vertex_by_data(self, data):
+    if self.is_representation_list:
+      return self._get_vertex_by_data_list(data)
+    else:
+      return self._get_vertex_by_data_matrix(data)
+  
+  def _vertex_degree_matrix(self, index):
+    for vertex in self.vertices_matrix:
+      if vertex.index == index:
+        return vertex.degree
+    raise IndexError("list index out of range")
+
+  def remove_edge(self, vertex1, vertex2):
+    if self.is_representation_list:
+      self._remove_edge_list(vertex1, vertex2)
+    else:
+      self._remove_edge_matrix(vertex1, vertex2)
+    
+  def _remove_edge_list(self, vertex1, vertex2):
+    if vertex1 in self.adjacency_list and vertex2 in self.adjacency_list:
+      if self.has_edge(vertex1, vertex2):
+        self._update_vertex_degrees(vertex1, vertex2, increment=False)
+        edge = (vertex1.data, vertex2.data)
+        if edge in self.edges_list:
+          self.edges_list.remove(edge)
+          self.adjacency_list[vertex1].remove(vertex2)
+          self.adjacency_list[vertex2].remove(vertex1)
+          self._edge_count -= 1
+      else:
+        raise ValueError("Edge does not exist in the graph.")
+    else:
+      raise ValueError("One or both vertices do not exist in the graph.")
+      
+  def _remove_edge_matrix(self, vertex1, vertex2):
+    if vertex1 in self.vertices_matrix and vertex2 in self.vertices_matrix:
+      if self.has_edge(vertex1, vertex2):
+        index1, index2 = self._get_vertices_matrix_index(vertex1, vertex2)
+        self._update_vertex_degrees(vertex1, vertex2, increment=False)
+        self._update_matrix_entries(index1, index2, increment=False)
+        edge = (vertex1.data, vertex2.data)
+        if edge in self.edges_matrix:
+          self.edges_matrix.remove(edge)
+          self._edge_count -= 1
+      else:
+        raise ValueError("Edge does not exist in the graph.")
+    else:
+      raise ValueError("One or both vertices do not exist in the graph.")
+  
+  def print_graph(self):
+    if self.is_representation_list:
+      self._print_list()
+    else:
+      self._print_matrix()
+
+  def _print_list(self):
+    print("Estrutura de adjacência")
+    print("Quantidade de vértices:", len(self.get_vertices()))
+    print("Quantidade de arestas:", self.get_edge_count())
+    print("Arestas:", self.get_edges())
+    for vertex in self.adjacency_list:
+      neighbors_data = [neighbor.data for neighbor in self.adjacency_list[vertex]]
+      print("Grau: {} | {} -> {}".format(vertex.degree, vertex.data, neighbors_data))
+  
+  def _print_matrix(self):
+    print("Matriz de adjacência")
+    print("Quantidade de vertices:", len(self.get_vertices()))
+    print("Quantidade de arestas:", self.get_edge_count())
+    print("Arestas:", self.get_edges())
+
+    print(" "*12, end="")
+    for vertex in self.vertices_matrix:
+      print(vertex.data, end=" ")
+    print()
+
+    for i in range(len(self.vertices_matrix)):
+      vertex = self.vertices_matrix[i]
+      print("Grau: {} | {}".format(vertex.degree, vertex.data), end=" ")
+      for j in range(len(self.vertices_matrix)):
+        print(self.matrix[i][j], end=" ")
+      print()
+    print()
+
+class Passeio:
+  def __init__(self):
+    self.k = 0
+    self._sequence = list()
+  
+  def get_sequence(self):
+    return self._sequence
+  
+  def add_component(self, component):
+    if isinstance(component, Vertex):
+      self._sequence.append(component)
+      self.k += 1
+    else:      
+      raise Exception("This component is neither a vertex nor an edge.")
+  
+def print_passeio(passeio):
+  first = True
+  for component in passeio.get_sequence():
+    if first:
+      print("{}".format(component.data), end="")
+      first = False
+    else:
+      print(", {}".format(component.data), end="")
+  print()
+
+def print_reversed_passeio(passeio):
+  components_data = [str(component.data) for component in passeio.get_sequence()]
+  reversed_data = reversed(components_data)
+  print(", ".join(reversed_data))
+
+def section_passeio(passeio, i, j):
+  sequence = passeio.get_sequence()
+  section = sequence[i:j+1]
+  if j + 1 > len(sequence):
+    raise IndexError("List index out of range")
+  print([vertex.data for vertex in section])
+  return section
+
+# passeio 
+graph = Graph("list")
+
+vertexX = Vertex("X", 1)
+vertexW = Vertex("W", 2)
+vertexV = Vertex("V", 3)
+vertexY = Vertex("Y", 4)
+vertexU = Vertex("U", 5)
+
+graph.add_vertex(vertexX)
+graph.add_vertex(vertexW)
+graph.add_vertex(vertexV)
+graph.add_vertex(vertexY)
+graph.add_vertex(vertexU)
+
+graph.add_edge(vertexX, vertexY)
+graph.add_edge(vertexX, vertexW)
+
+graph.add_edge(vertexW, vertexY)  
+graph.add_edge(vertexW, vertexV)
+  
+graph.add_edge(vertexY, vertexV) 
+graph.add_edge(vertexY, vertexV) 
+graph.add_edge(vertexY, vertexU)
+  
+graph.add_edge(vertexV, vertexU) 
+
+passeio = Passeio()
+passeio.add_component(vertexX)
+passeio.add_component(vertexY)
+passeio.add_component(vertexW)
+passeio.add_component(vertexV)
+print_passeio(passeio)
+print_reversed_passeio(passeio)
+section_passeio(passeio, 0, 4)
+
+
